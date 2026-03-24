a2eb083f16de2a3dd5e027ae68fec1890782e0fb
zumarawan <zumar.awan@aigros.com>
2026-03-24 13:25:33 +0500

    feat: crud implemented

--- FormPractice/index.js
+++ FormPractice/index.js
@@ -1,24 +1,51 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.sendFile(path.join(__dirname, "Form", "index.html"));
 });
 
-app.get("/register", (req, res) => {
-  const { username, password } = req.query;
-  res.send(`Register GET Back ${username}`);
+let users = [{ id: 1, username: "Ali", password: "123" }];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "Form")));
+//Get By Id
+app.get("/register/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id == id);
+  res.json(user);
 });
 
+//  GET USERS
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+//  REGISTER
 app.post("/register", (req, res) => {
-  console.log("Post Request Received", req.query);
-
-  const { username, password } = req.query;
-
-  res.send(`Register POST  ${username}`);
+  let { username, password } = req.body;
+  const lastUser = users[users.length - 1];
+  const newId = lastUser ? lastUser.id + 1 : 1;
+  users.push({ id: newId, username, password });
+  res.sendFile(path.join(__dirname, "Form", "users.html"));
 });
-
+//Delete USER
+app.delete("/register/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id != id);
+  res.json(users);
+});
+//Update User
+app.patch("/register/:id", (req, res) => {
+  const { id } = req.params;
+  const { username, password } = req.body;
+  let user = users.find((user) => user.id == id);
+  user.username = username;
+  user.password = password;
+  res.sendFile(path.join(__dirname, "Form", "users.html"));
+});
+//LISTEN
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
